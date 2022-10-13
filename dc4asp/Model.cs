0bc32a25356f1102440286e88d2da4dd7f6a7272
Wojciech Wieczorek <wwieczorek@ath.bielsko.pl>
2022-10-13 23:34:53 +0200

    Start an idea

--- dc4asp/Model.cs
+++ dc4asp/Model.cs
@@ -10,10 +10,39 @@ namespace dc4asp
         // part of a body), negative numbers (atoms with not).
         // The length of a rule is at least 1. Constraints are created
         // with 1 in the head, and -1 at the end.
-        
+
         public Model()
         {
             rules = new();
+        }
+
+        private List<ImmutableList<int>> computeReduct(ImmutableHashSet<int> set)
+        {
+            var reduct = new List<ImmutableList<int>>();
+            foreach (var rule in rules)
+            {
+                bool to_remove = false;
+                foreach (int a in rule)
+                {
+                    if (a < 0)
+                    {
+                        if (set.Contains(-a))
+                        {
+                            to_remove = true;
+                            break;
+                        }
+                    }
+                }
+                if (!to_remove)
+                {
+                    reduct.Add(rule);
+                }
+            }
+            for (int i = 0; i < reduct.Count; ++i)
+            {
+                reduct[i] = reduct[i].Where(a => a > 0).ToImmutableList();
+            }
+            return reduct;
         }
 
         private bool isStable(ImmutableHashSet<int> set)
@@ -25,6 +54,7 @@ namespace dc4asp
 
         public IEnumerable<ImmutableHashSet<int>> AnswerSets()
         {
+            rules.Shuffle();
             foreach(ImmutableList<int> answer in DivideAndMerge(0, rules.Count - 1))
             {
                 var set = answer.Where(a => a > 0).ToImmutableHashSet();
