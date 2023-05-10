3a09153135c0ac89956e5e27a6ccc8fb20d1dd92
jozefsalik <jozek243@gmail.com>
2023-05-10 09:02:51 +0200

    Add FileReader class.

---
+++ dc4asp/Helpers/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace dc4asp.Helpers
+{
+    internal static class FileReader
+    {
+        public static string[] ReadFile(string path)
+        {
+            var file = File.ReadAllText(path);
+            string pattern = @"(?<!\.)\.(?!\.)"; // Matches a single period not followed by another period
+            return Regex.Split(file, pattern).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+        }
+    }
+}
