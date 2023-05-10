32377bf2c6eb12be282b9ab56cf784885cabf871
jozefsalik <jozek243@gmail.com>
2023-05-10 09:01:42 +0200

    Create constants from variables.

---
+++ dc4asp/Parser.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.RegularExpressions;
+
+namespace dc4asp
+{
+    internal static class Parser
+    {
+        public static List<string> VariablesToConstants(string[] lines)
+        {
+            List<string> result = new();
+
+            foreach (var line in lines)
+            {
+                if (!line.Contains(":-"))
+                {
+                    if (!line.Contains('(') && !line.Contains(')'))
+                    {
+                        result.Add(line);
+                        continue;
+                    }
+
+                    string varName = GetName(line);
+                    string varValue = GetValue(line);
+
+                    if (varValue.Contains(".."))
+                    {
+                        List<int> values = ParseRange(varValue);
+                        foreach (int value in values)
+                        {
+                            result.Add($"{varName}({value})");
+                        }
+                    }
+                    else
+                    {
+                        result.Add(line);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static string GetName(string line)
+        {
+            string pattern = @"(.*?)\(";
+            Match match = Regex.Match(line, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            throw new Exception("Getting variable name failed.");
+        }
+
+        private static string GetValue(string line)
+        {
+            string pattern = @"\((.*?)\)";
+            Match match = Regex.Match(line, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            throw new Exception("Getting variable value failed.");
+        }
+
+        private static List<int> ParseRange(string range)
+        {
+            string[] parts = range.Split(new[] { ".." }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 2 || !int.TryParse(parts[0], out int start) || !int.TryParse(parts[1], out int end))
+            {
+                throw new Exception("Invalid range format.");
+            }
+
+            List<int> values = new();
+            for (int i = start; i <= end; i++)
+            {
+                values.Add(i);
+            }
+
+            return values;
+        }
+    }
+}
--- dc4asp/Program.cs
+++ dc4asp/Program.cs
@@ -1,49 +1,56 @@
 ﻿using dc4asp;
-using System.Collections.Immutable;
+using dc4asp.Helpers;
+
+
+
+
+
+
+
+
+var lines = FileReader.ReadFile("C:\\Users\\jozek\\OneDrive\\mgr\\bloki.lp");
+
+var fdfd = Parser.VariablesToConstants(lines);
+
 
 Model model = new();
-int b, i, j, idx = 1, n = 12;
-Dictionary<(int, int), int> block = new();
-for (i = 1; i <= n; ++i)
-{
-    for (b = 1; b <= 3; ++b)
-    {
-        block[(i, b)] = idx++;
-    }
-}
-for (i = 1; i <= n; ++i)
-{
-    model.rules.Add(ImmutableList.Create(block[(i, 1)], -block[(i, 2)], -block[(i, 3)]));
-    model.rules.Add(ImmutableList.Create(block[(i, 2)], -block[(i, 1)], -block[(i, 3)]));
-    model.rules.Add(ImmutableList.Create(block[(i, 3)], -block[(i, 1)], -block[(i, 2)]));
-    model.rules.Add(ImmutableList.Create(0, -block[(i, 1)], -block[(i, 2)], -block[(i, 3)]));
-}
-for (i = 1; i <= n; ++i)
-{
-    for (j = i; j <= n; ++j)
-    {
-        if (i + j <= n)
-        {
-            for (b = 1; b <= 3; ++b)
-            {
-                model.rules.Add(ImmutableList.Create(0, block[(i, b)], block[(j, b)], block[(i + j, b)]));
-            }
-        }
-    }
-}
-var answer = model.AnswerSets(idx - 1).FirstOrDefault();
-HashSet<int>[] partition = new HashSet<int>[4];
-for (b = 1; b <= 3; ++b)
-{
-    partition[b] = new();
-}
-foreach (((int number, int block_idx), int var_idx) in block)
-{
-    if (answer!.Contains(var_idx))
-    {
-        partition[block_idx].Add(number);
-    }
-}
-Console.WriteLine($"Block 1: {partition[1].AsText()}");
-Console.WriteLine($"Block 2: {partition[2].AsText()}");
-Console.WriteLine($"Block 3: {partition[3].AsText()}");
+//int b, i, j, idx = 1, n = 4;
+//Dictionary<string, int> dictionary = new();
+
+//for (i = 1; i <= n; ++i)
+//{
+//    model.rules.Add(ImmutableList.Create(block[(i, 1)], -block[(i, 2)], -block[(i, 3)]));
+//    model.rules.Add(ImmutableList.Create(block[(i, 2)], -block[(i, 1)], -block[(i, 3)]));
+//    model.rules.Add(ImmutableList.Create(block[(i, 3)], -block[(i, 1)], -block[(i, 2)]));
+//    model.rules.Add(ImmutableList.Create(0, -block[(i, 1)], -block[(i, 2)], -block[(i, 3)]));
+//}
+//for (i = 1; i <= n; ++i)
+//{
+//    for (j = i; j <= n; ++j)
+//    {
+//        if (i + j <= n)
+//        {
+//            for (b = 1; b <= 3; ++b)
+//            {
+//                model.rules.Add(ImmutableList.Create(0, block[(i, b)], block[(j, b)], block[(i + j, b)]));
+//            }
+//        }
+//    }
+//}
+
+//var answer = model.AnswerSets(idx - 1).FirstOrDefault();
+//HashSet<int>[] partition = new HashSet<int>[4];
+//for (b = 1; b <= 3; ++b)
+//{
+//    partition[b] = new();
+//}
+//foreach (((int number, int block_idx), int var_idx) in block)
+//{
+//    if (answer!.Contains(var_idx))
+//    {
+//        partition[block_idx].Add(number);
+//    }
+//}
+//Console.WriteLine($"Block 1: {partition[1].AsText()}");
+//Console.WriteLine($"Block 2: {partition[2].AsText()}");
+//Console.WriteLine($"Block 3: {partition[3].AsText()}");
